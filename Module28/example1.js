a730967dd6ae04516a0e5c382e3df72bc53670d0
altin664 <altin.krasniqi210@gmail.com>
2025-03-24 19:48:13 +0100

    events

---
+++ Module28/example1.js
@@ -0,0 +1,9 @@
+var btn1 = document.getElementById('btn1')
+
+btn1.addEventListener('click' ,function () {
+alert('HELLO!!');
+
+});
+
+ourElement.addEventListener('click' ,function1);
+ourElement.addEventListener('click' ,function2);
