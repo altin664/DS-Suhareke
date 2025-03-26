103c7071ae15ee7572da37bcd845d5997ea84e6a
altin664 <altin.krasniqi210@gmail.com>
2025-03-26 19:18:35 +0100

    medium

---
+++ Module28/medium.js
@@ -0,0 +1,7 @@
+btn1.onclick = function () {
+    txt1.style.color="green";
+    txt1.style.backgroundColor="yellow";
+    txt1.style.textAlign="center";
+    txt1.style.fontSize="24px";
+    txt1.style.padding="20px";
+}
