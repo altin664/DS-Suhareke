6b2340bfa3738c394bc4d88f2d519ec65f6662cb
altin664 <altin.krasniqi210@gmail.com>
2025-03-26 19:51:15 +0100

    hard

---
+++ Module28/hard.js
@@ -0,0 +1,28 @@
+var btn_circle = document.getElementById("btnC");
+var btn_rect=document.getElementById("btnR")
+var btn_triangle=document.getElementById("btnT")
+
+
+var circle=document.getElementById("circle");
+var rect=document.getElementById("rect");
+var triangle=document.getElementById("triangle");
+
+btn_circle.onclick=function() {
+    circle.setAttribute("class", "shape_circle");
+}
+btn_rect.onclick=function() {
+    rect.setAttribute("class", "shape_rect");
+}
+btn_triangle.onclick=function() {
+    triangle.setAttribute("class", "shape_triangle");
+}
+
+circle.onclick =function() {
+    circle.setAttribute("class", "hide");
+}
+rect.onclick =function() {
+    rect.setAttribute("class", "hide");
+}
+triangle.onclick =function() {
+    triangle.setAttribute("class", "hide");
+}
