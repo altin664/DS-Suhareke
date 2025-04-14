185dd882f9ebd2bbc060dffdf2eac22258de9666
altin664 <altin.krasniqi210@gmail.com>
2025-04-14 19:14:24 +0200

    module 30

---
+++ Module30/condition.js
@@ -0,0 +1,32 @@
+var input = document.getElementById('input_id')
+var input = document.getElementById('btn_id')
+var input = document.getElementById('input_Id')
+var input = document.getElementById('text_id')
+
+//button.onclick = function(){
+  //  text.innerHTML = input.value;
+//}
+
+var input = document.querySelector('#input1_id')
+var input = document.querySelector('#input2_id')
+var input = document.querySelector('#btn2_id')
+var input = document.querySelector('#answer')
+
+btn2.addEventListener("click",function(){
+    ans.innerHTML = parseInt(input1.value) + parseInt(input2.value);
+});
+
+button.onclick = function (){
+    if(input.value > 10)
+    {
+        text.innerHTML="Input value is greater then 10";
+    }
+    else if( input.value < 10){
+        text.innerHTML = "Input value is less than 10";
+
+    }
+    else{
+        text.innerHTML = "Input value is equal to 10";
+    }
+    
+}
