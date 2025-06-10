c6f728df566df7ff88d23484c1ccfeb13253af46
altin664 <altin.krasniqi210@gmail.com>
2025-06-10 18:44:57 +0200

    32

---
+++ Module32/medium/medium.js
@@ -0,0 +1,13 @@
+function validateAge() {
+    const ageInput = document.getElementById('age').value;
+    const errorSpan = document.getElementById('error');
+    const agePattern = /^[A-Za-z]+$/;
+  
+    if (!agePattern.test(ageInput)) {
+      errorSpan.textContent = "Only Numbers are allowed.";
+      return false;
+    } else {
+      errorSpan.textContent = "";
+      return true;
+    }
+  }
